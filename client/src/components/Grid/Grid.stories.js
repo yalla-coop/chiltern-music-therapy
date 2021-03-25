a9b0e1a8daa9abeb3a05e73112a9c5f9ea7b13bd
fadi <fadeomar2015@gmail.com>
2021-03-25 20:11:31 +0200

    update Grid component

--- client/src/components/Grid/Grid.stories.js
+++ client/src/components/Grid/Grid.stories.js
@@ -28,3 +28,22 @@ Default.args = {
   inner: true,
   jc: 'flex-start',
 };
+const FlexCols = (args) => (
+  <Row inner={args.inner} jc={args.jc}>
+    <Col w={[4, 4, 4]}>
+      <p style={{ background: 'red', flex: 1 }}>COl 1</p>
+    </Col>
+    <Col w={[4, 4, 4]}>
+      <p style={{ background: 'green', flex: 1 }}>COl 2</p>
+    </Col>
+    <Col w={[4, 4, 4]}>
+      <p style={{ background: 'orange', flex: 1 }}>COl 3</p>
+    </Col>
+  </Row>
+);
+
+export const FlexColumns = FlexCols.bind({});
+Default.args = {
+  inner: true,
+  jc: 'flex-start',
+};
