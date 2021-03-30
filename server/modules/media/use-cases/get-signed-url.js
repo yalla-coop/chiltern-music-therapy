7a06c0c8806cf618e179f6b9f192b86ce8fcfd55
Simon Dupree <dupreenator@gmail.com>
2021-03-30 22:06:06 +0200

    updated file params

--- server/modules/media/use-cases/get-signed-url.js
+++ server/modules/media/use-cases/get-signed-url.js
@@ -1,12 +1,5 @@
 import { getSignedURL as getSignedURLService } from '../../../services/files-storage';
 
-/**
- *  create new image in db only
- * @param {userId} param0 image owner user id
- * @param {fileName} param0 the original file name
- * @param {imageType} param0 image type (cover | logo | activity)
- * @param {fileType} param0 original fileType (image/jpg | image/png | ...)
- */
 const getSignedURL = async ({
   userId,
   fileType,
