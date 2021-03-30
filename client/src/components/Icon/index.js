974f9c1c5558b68ab07850748c6e21bc65cd57ac
fadi <fadeomar2015@gmail.com>
2021-03-30 18:17:37 +0300

    upload user icon

--- client/src/components/Icon/index.js
+++ client/src/components/Icon/index.js
@@ -2,12 +2,14 @@ import theme from './../../theme';
 
 // ICONS
 import Email from './icons/Email';
+import User from './icons/User';
 
 const Icon = (props) => {
   const { icon, color } = props;
 
   const IconMap = {
     email: Email,
+    user: User,
   };
 
   const StyledIcon = IconMap[icon];
