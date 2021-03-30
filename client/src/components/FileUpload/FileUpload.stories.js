cf9ec9fa7f9006ba2b4b7b92563fadebd223a899
Simon Dupree <dupreenator@gmail.com>
2021-03-30 20:12:26 +0200

    added storybook

--- client/src/components/FileUpload/FileUpload.stories.js
+++ client/src/components/FileUpload/FileUpload.stories.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import FileUpload from '.';
 
@@ -14,19 +14,52 @@ import FileUpload from '.';
 
 export default {
   title: 'Common Components/File Upload',
-  component: FileUpload,
-  argTypes: {
-    category: { control: 'string' },
-    fileUploadError: { control: 'string' },
-    uploadedFileInfo: { control: 'object' },
-  },
+  argTypes: {},
 };
 
-const Template = (args) => <FileUpload {...args} />;
+const Template = (args) => {
+  const [fileUploadError, setFileUploadError] = useState(null);
+  const [uploadedFileInfo, setUploadedFileInfo] = useState({});
+  const [fileUploading, setFileUploading] = useState(false);
 
-export const Normal = Template.bind({});
-Normal.args = {
-  icon: 'email',
-  width: 50,
-  height: 50,
+  return (
+    <div>
+      <FileUpload
+        {...args}
+        fileUploadError={fileUploadError}
+        handleFileUploadError={setFileUploadError}
+        handleFileUploadInfo={setUploadedFileInfo}
+        uploadedFileInfo={uploadedFileInfo}
+        fileUploading={fileUploading}
+        handleFileUploadStatus={setFileUploading}
+      />
+
+      <div style={{ border: '2px solid', marginTop: '2rem' }}>
+        <h3>Try to upload a file</h3>
+        <p>when selecting a correct file it won't actually upload...</p>
+
+        <h4>accepted file types:</h4>
+        <ul>
+          <li>video: mp4 / less than 2GB</li>
+          <li>document: PDF / less than 20MB</li>
+          <li>audio: mpeg / less than 100MB</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export const VideoUpload = Template.bind({});
+VideoUpload.args = {
+  category: 'video',
+};
+
+export const DocumentUpload = Template.bind({});
+DocumentUpload.args = {
+  category: 'application',
+};
+
+export const AudioUpload = Template.bind({});
+AudioUpload.args = {
+  category: 'audio',
 };
