c6e7624633030e08e2c5d28d0867424834f8e230
fadi <fadeomar2015@gmail.com>
2021-03-30 15:53:59 +0300

    update layout component with new layouts

--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Onboarding from './Onboarding';
+import Info from './Info';
 import General from './General';
 
 const Layout = ({ layout, ...props }) => {
   switch (layout) {
+    case 'onboarding':
+      return <Onboarding {...props} />;
+    case 'info':
+      return <Info {...props} />;
     case 'general':
     default:
       return <General {...props} />;
