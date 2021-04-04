333e876560c0fdff8f2f66e9635e6062d5aafa32
fadi <fadeomar2015@gmail.com>
2021-04-04 23:46:42 +0300

    add fake data for client dashboard

--- client/src/pages/Client/Dashboard/index.js
+++ client/src/pages/Client/Dashboard/index.js
@@ -1,52 +1,85 @@
 import { CLIENT } from '../../../constants/nav-routes';
-import { useHistory } from 'react-router';
 import { Col, Row } from '../../../components/Grid';
 import * as T from '../../../components/Typography';
+import { Link, Basic } from '../../../components/Cards';
+import moment from 'moment';
 
 import * as S from './style';
 
 const Dashboard = () => {
-  const history = useHistory();
+  const data = {
+    firstName: 'Joe',
+    lastName: 'Petter',
+    therapistInfo: {
+      firstName: 'Elizabeth',
+      lastName: 'Peters',
+      id: '1',
+    },
+    programme: { date: moment(), id: 0 },
+  };
+  const firstLatter = data.firstName[0].toUpperCase();
+  const lastLatter = data.lastName[0].toUpperCase();
   return (
     <div style={{ maxWidth: 1068 }}>
       <Row>
         <Col w={[4, 12, 12]}>
           <T.H2>
-            Welcome <S.BoldSpan> J </S.BoldSpan> P!
+            Welcome <S.BoldSpan> {firstLatter} </S.BoldSpan> {lastLatter}!
           </T.H2>
         </Col>
       </Row>
       <Row>
         <Col w={[4, 6, 4]} mt={5}>
-          <div style={{ width: '100%', height: 100, background: 'gray' }}>
-            <T.P>My therapist</T.P>
-            <T.H4 bold>Elizabeth Peters</T.H4>
-            <T.Link to={CLIENT.THERAPIST} underline>
-              View biography
-            </T.Link>{' '}
-            <T.Link to={CLIENT.CONTACT_THERAPIST} underline>
-              Contact my therapist
-            </T.Link>
-          </div>
+          <S.CardWrapper>
+            <Basic therapistInfo={data.therapistInfo} variant="therapistInfo" />
+          </S.CardWrapper>
         </Col>
         <Col w={[4, 6, 4]} mt={5}>
-          <div
-            style={{ width: '100%', height: 100, background: 'blue' }}
-            onClick={() => history.push(CLIENT.THERAPY_PLAN)}
-          >
-            my therapy plan
-          </div>
+          <S.CardWrapper>
+            <Link
+              variant="graphic1"
+              title="therapy plan"
+              to={CLIENT.THERAPY_PLAN}
+            />
+          </S.CardWrapper>
         </Col>
         <Col w={[4, 6, 4]} mt={5}>
-          <div
-            style={{ width: '100%', height: 100, background: 'blue' }}
-            onClick={() => history.push(CLIENT.THERAPY_GOALS)}
-          >
-            my therapy goals
-          </div>
+          <S.CardWrapper>
+            <Link
+              variant="graphic2"
+              title="therapy goals"
+              to={CLIENT.THERAPY_GOALS}
+            />
+          </S.CardWrapper>
         </Col>
       </Row>
-      <Row></Row>
+      <Row mt={8}>
+        <Col w={[4, 12, 12]}>
+          <T.P>
+            My <S.BoldSpan>home programmes</S.BoldSpan>
+          </T.P>
+        </Col>
+      </Row>
+      <Row mt={6}>
+        <Col w={[4, 4, 4]}>
+          {data?.programme?.date ? (
+            <>
+              <S.CardWrapper>
+                <Link
+                  variant="programme"
+                  programme={data.programme}
+                  to={CLIENT.PROGRAMME.replace(':id', data.programme.id)}
+                />
+              </S.CardWrapper>
+              <T.Link to={CLIENT.PROGRAMMES} weight="bold" mt={7}>
+                View more
+              </T.Link>
+            </>
+          ) : (
+            <Basic variant="noProgrammes" />
+          )}
+        </Col>
+      </Row>
     </div>
   );
 };
