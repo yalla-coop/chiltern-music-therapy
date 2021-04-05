abc7f3e543bc3a01c56518520a8af6ac5603efed
fadi <fadeomar2015@gmail.com>
2021-04-05 12:48:35 +0300

    update the therapist dashboard

--- client/src/pages/Therapist/Dashboard/index.js
+++ client/src/pages/Therapist/Dashboard/index.js
@@ -1,25 +1,14 @@
-import { CLIENT, THERAPIST } from '../../../constants/nav-routes';
+import { THERAPIST } from '../../../constants/nav-routes';
 import { Col, Row } from '../../../components/Grid';
 import * as T from '../../../components/Typography';
-import { Link, Basic } from '../../../components/Cards';
-import { BasicInput as SearchInput } from '../../../components/Inputs';
+import { Link } from '../../../components/Cards';
+
 import moment from 'moment';
 
 import * as S from './style';
-import { useState } from 'react';
-
-const decideBorderColor = (n) => {
-  if (n > 3) {
-    return decideBorderColor(n - 3);
-  } else {
-    if (n === 1) return 'rainbowHorizontal';
-    if (n === 2) return 'Blue';
-    if (n === 3) return 'PinkUnder';
-  }
-};
+import ClientsSection from './ClientsSection';
 
 const Dashboard = () => {
-  const [search, setSearch] = useState('');
   const data = {
     firstName: 'lex',
     clients: Array(15).fill({
@@ -36,13 +25,6 @@ const Dashboard = () => {
     programme: { date: moment(), id: 0 },
   };
 
-  console.log(decideBorderColor(10));
-  const handleChange = (e) => {
-    const {
-      target: { value },
-    } = e;
-    setSearch(value);
-  };
   return (
     <div style={{ maxWidth: 1068 }}>
       <Row>
@@ -53,83 +35,21 @@ const Dashboard = () => {
         </Col>
       </Row>
       <Row>
-        <Col w={[4, 12, 12]} mt={7}>
+        <Col w={[4, 12, 12]} mt={7} mtT={5}>
           <T.H3>My Clients</T.H3>
         </Col>
       </Row>
-      <Row>
-        <Col w={[4, 4, 4]} mt={6}>
-          <SearchInput
-            placeholder="Search..."
-            value={search}
-            onChange={handleChange}
-          />
-        </Col>
-      </Row>
-      <Row mt={5}>
-        {data.clients.map((client, index) => {
-          return (
-            <Col w={[4, 4, 4]} mt={4}>
-              <S.CardWrapper>
-                <Link
-                  variant="client"
-                  title="therapy plan"
-                  client={{
-                    firstInitial: 'J',
-                    secondInitial: 'P',
-                    postcode: 'SW',
-                    id: 1,
-                  }}
-                  to={THERAPIST.CLIENT.replace(':id', client.id)}
-                  borderColor={decideBorderColor(index + 1)}
-                />
-              </S.CardWrapper>
-            </Col>
-          );
-        })}
-      </Row>
-      <Row>
+      <ClientsSection clients={data.clients} />
+      <Row mt={9} mtT={8}>
         <Col w={[4, 6, 4]} mt={5}>
           <S.CardWrapper>
-            <Basic therapistInfo={data.therapistInfo} variant="therapistInfo" />
+            <Link variant="graphic1" title="library" to={THERAPIST.LIBRARY} />
           </S.CardWrapper>
         </Col>
-        <Col w={[4, 6, 4]} mt={5}></Col>
         <Col w={[4, 6, 4]} mt={5}>
           <S.CardWrapper>
-            <Link
-              variant="graphic2"
-              title="therapy goals"
-              to={CLIENT.THERAPY_GOALS}
-            />
+            <Link variant="graphic2" title="profile" to={THERAPIST.ACCOUNT} />
           </S.CardWrapper>
-        </Col>
-      </Row>
-      <Row mt={8}>
-        <Col w={[4, 12, 12]}>
-          <T.P>
-            My <S.BoldSpan>home programmes</S.BoldSpan>
-          </T.P>
-        </Col>
-      </Row>
-      <Row mt={6}>
-        <Col w={[4, 4, 4]}>
-          {data?.programme?.date ? (
-            <>
-              <S.CardWrapper>
-                <Link
-                  variant="programme"
-                  programme={data.programme}
-                  to={CLIENT.PROGRAMME.replace(':id', data.programme.id)}
-                />
-              </S.CardWrapper>
-              <T.Link to={CLIENT.PROGRAMMES} weight="bold" mt={7}>
-                View more
-              </T.Link>
-            </>
-          ) : (
-            <Basic variant="noProgrammes" />
-          )}
         </Col>
       </Row>
     </div>
