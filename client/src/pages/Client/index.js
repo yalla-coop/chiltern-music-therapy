60436316e8e038118b798575d184e45d264d3f76
fadi <fadeomar2015@gmail.com>
2021-04-05 20:24:27 +0300

    export the new pages

--- client/src/pages/Client/index.js
+++ client/src/pages/Client/index.js
@@ -1,5 +1,13 @@
 import AllProgrammes from './AllProgrammes';
+import MyTherapyPlan from './MyTherapyPlan';
+import MyTherapyGoals from './MyTherapyGoals';
 import Dashboard from './Dashboard';
 import Signup from './Signup';
 
-export default { Dashboard, Signup, AllProgrammes };
+export default {
+  Dashboard,
+  Signup,
+  AllProgrammes,
+  MyTherapyPlan,
+  MyTherapyGoals,
+};
