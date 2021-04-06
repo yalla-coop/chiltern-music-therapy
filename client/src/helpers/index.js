d897a8a90abf0240cc88cd6f5061645247e00e34
fadi <fadeomar2015@gmail.com>
2021-04-06 14:48:16 +0300

    create helper for clean Email

--- client/src/helpers/index.js
+++ client/src/helpers/index.js
@@ -1,5 +1,6 @@
 import authorization from './authorization';
 import setMargin from './set-margin';
 import dateFormatter from './dateFormatter';
+import cleanEmail from './cleanEmail';
 
-export { authorization, setMargin, dateFormatter };
+export { authorization, setMargin, dateFormatter, cleanEmail };
