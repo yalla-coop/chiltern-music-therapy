7fb93df5f1c95968f193be4b7650d71a3698e5c1
fadi <fadeomar2015@gmail.com>
2021-04-07 16:46:19 +0300

    create step3

--- client/src/pages/Therapist/AddClient/Step3.js
+++ client/src/pages/Therapist/AddClient/Step3.js
@@ -2,27 +2,29 @@ import { useState } from 'react';
 
 import { Row, Col } from '../../../components/Grid';
 import * as T from '../../../components/Typography';
-import { BasicInput } from '../../../components/Inputs';
+import { Textarea } from '../../../components/Inputs';
 import Button from '../../../components/Button';
-import { addClient as validate } from '../../../validation/schemas';
+import { TherapyGoals } from '../../../components/Cards';
+import { step3 as validate } from '../../../validation/schemas/addClient';
+import Example from '../../../components/Example';
 
 const Step3 = ({ submitStep }) => {
-  const [email, setEmail] = useState('');
-  const [mobileNumber, setMobileNumber] = useState('');
-  const [PrimaryMobileNumber, setPrimaryMobileNumber] = useState('');
-  const [errors, setErrors] = useState('');
+  const [background, setBackground] = useState('');
+  const [goals, setGoals] = useState([{ goal: '', category: '', id: 0 }]);
+
+  const [errors, setErrors] = useState({});
 
   const handleClick = () => {
     try {
       validate({
-        email,
-        mobileNumber,
-        PrimaryMobileNumber,
+        background,
+        goals,
       });
       setErrors({});
-      submitStep({ email, mobileNumber, PrimaryMobileNumber });
+      submitStep({ background, goals });
       return true;
     } catch (error) {
+      console.log({ error });
       if (error.name === 'ValidationError') {
         setErrors({ ...error.inner });
       }
@@ -42,38 +44,51 @@ const Step3 = ({ submitStep }) => {
       </Row>
       <Row mt={6}>
         <Col w={[4, 4, 4]}>
-          <BasicInput
-            label="Email"
-            placeholder="Email..."
-            value={email}
-            handleChange={setEmail}
-            name="email"
-            error={errors.email}
+          <Textarea
+            label="Therapy background "
+            placeholder="Therapy background..."
+            value={background}
+            handleChange={setBackground}
+            name="background"
+            error={errors.background}
+            rows={5}
+            mb={4}
           />
+          <Example showLabel="Example text">
+            <T.P color="gray8">
+              Between our therapy sessions and home programme content, we’ll
+              continue supporting J P SW in 4 key areas: social interaction &
+              communication, emotional wellbeing, cognition; learning, and
+              physical skills. Each week, we’ll focus on 1 or more of these
+              areas, with an accompanying resource to support her over that
+              week.
+            </T.P>
+          </Example>
         </Col>
         <Col w={[4, 4, 4]}>
-          <BasicInput
-            label="Mobile number"
-            placeholder="Mobile number (optional)"
-            value={mobileNumber}
-            handleChange={setMobileNumber}
-            name="mobileNumber"
-            error={errors.mobileNumber}
+          <TherapyGoals
+            goals={goals}
+            handleChange={setGoals}
+            label="Therapy goals"
+            error={errors.goals}
           />
+          <Example showLabel="Example text">
+            <T.P bold>Communication</T.P>
+            <T.P color="gray8">
+              - Goal 1: JE will consistently follow lyrical cues to wait and
+              turn-take
+            </T.P>
+            <T.P bold mt="5">
+              Emotional wellbeing
+            </T.P>
+            <T.P color="gray8">
+              - Goal 1: For JE to be able to use eye gaze or visuals to
+              communicate how she is feeling.
+            </T.P>
+          </Example>
         </Col>
       </Row>
-      <Row mt={6}>
-        <Col w={[4, 4, 4]}>
-          <BasicInput
-            label="Primary contact mobile number"
-            placeholder="Mobile number"
-            value={PrimaryMobileNumber}
-            handleChange={setPrimaryMobileNumber}
-            name="PrimaryMobileNumber"
-            error={errors.PrimaryMobileNumber}
-          />
-        </Col>
-      </Row>
+
       <Row mt={6}>
         <Col w={[4, 4, 4]}>
           <Button text="Next" handleClick={handleClick} />
