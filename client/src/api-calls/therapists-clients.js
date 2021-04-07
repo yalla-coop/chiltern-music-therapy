bb49fa1db2d4e370276d7698f7dad25a42f0866c
Fadi Omar <39485495+fadeomar@users.noreply.github.com>
2021-04-07 10:55:51 +0300

    47 therapist can add profile(front end) (#112)
    * create validation for profile page
    
    * create helper for clean Email
    
    * setup the api call for profile page
    
    * create profile page
    
    * add the profile page to the app
    
    * remove unused imprting component
    
    * remove console
    
    * remove console
    
    * fix styling
    
    * update the text ob save button

--- client/src/api-calls/therapists-clients.js
+++ client/src/api-calls/therapists-clients.js
@@ -36,6 +36,18 @@ const getTherapistByInviteToken = async ({ inviteToken, options }) => {
   }
 };
 
+const updateTherapiesProfile = async ({ updates, options } = {}) => {
+  try {
+    // const { data } = await axios.post(
+    //   `${THERAPISTS_CLIENTS_BASE}/update-therapies-profile`
+    // )
+    // return data;
+    return { data: updates };
+  } catch (error) {
+    const err = handleError(error, options);
+    return { error: err };
+  }
+};
 const getMyTherapy = async ({ options } = {}) => {
   try {
     const { data } = await axios.get(`${THERAPISTS_CLIENTS_BASE}/my-therapy`);
@@ -49,6 +61,7 @@ const getMyTherapy = async ({ options } = {}) => {
 export {
   getTherapistsClientsById,
   getTherapistByInviteToken,
-  getMyTherapy,
   getMyTherapist,
+  updateTherapiesProfile,
+  getMyTherapy,
 };
--- client/src/pages/Therapist/index.js
+++ client/src/pages/Therapist/index.js
@@ -1,5 +1,6 @@
 import CreateProgram from './CreateProgram';
 import Signup from './Signup';
 import Dashboard from './Dashboard';
+import Profile from './Profile';
 
-export default { CreateProgram, Signup, Dashboard };
+export default { CreateProgram, Signup, Dashboard, Profile };
