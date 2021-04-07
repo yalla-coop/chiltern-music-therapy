5ba8d34dd73063d423f9cc56ea126a6eea55dd89
Ramy Shurafa <ramyshurafa@hotmail.com>
2021-04-07 13:20:35 +0300

    format dates

--- server/services/emails/utils.js
+++ server/services/emails/utils.js
@@ -1,9 +1,11 @@
+import moment from 'moment';
+
 const htmlLink = (href, text) => {
   return `<a href="${href}">${text}</a>`;
 };
 
 const formateDate = (date) => {
-  return date; // TODO: use moment
+  return moment(date).format('DD/MM/YYYY');
 };
 
 export { htmlLink, formateDate };
