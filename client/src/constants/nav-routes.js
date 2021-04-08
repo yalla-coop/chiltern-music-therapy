ba376e077705f7dbf6c07c753ee413cc5343b555
fadi <fadeomar2015@gmail.com>
2021-04-08 16:44:27 +0300

    add new routes for therapist

--- client/src/constants/nav-routes.js
+++ client/src/constants/nav-routes.js
@@ -40,6 +40,7 @@ const THERAPIST = {
   CREATE_PROGRAMME_CONTENT_HOW_TO_RECORD: `${THERAPIST_BASE}/clients/:id/programme/create/content/how-to-record`,
   CREATE_PROGRAMME_REVIEW: `${THERAPIST_BASE}/clients/:id/programme/create/review`,
   CREATE_PROGRAMME_SUCCESS: `${THERAPIST_BASE}/clients/:id/programme/create/success`,
+  EDIT_PROGRAMME: `${THERAPIST_BASE}/clients/:id/programme/edit`,
   CLIENT: `${THERAPIST_BASE}/clients/:id`,
   CLIENT_HISTORY: `${THERAPIST_BASE}/clients/:id/history`,
   CONTACT_CLIENT: `${THERAPIST_BASE}/clients/:id/contact`,
@@ -47,6 +48,7 @@ const THERAPIST = {
   NEW_CLIENT: `${THERAPIST_BASE}/new-client`,
   SINGLE_PROGRAMME: `${THERAPIST_BASE}/programmes/:id`,
   PROFILE: `${THERAPIST_BASE}/profile`,
+  MESSAGE: `${THERAPIST_BASE}/message`,
 };
 
 const ADMIN_BASE = '/admin';
