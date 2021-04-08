8a583818dca50bf26ae74a42969f5ead1bfc3ae3
Joe Friel <joseph.s.friel@gmail.com>
2021-04-08 14:57:27 +0100

    Update AccountDeleted.js

--- client/src/pages/Client/DeleteAccount/AccountDeleted.js
+++ client/src/pages/Client/DeleteAccount/AccountDeleted.js
@@ -9,11 +9,11 @@ import { navRoutes } from '../../../constants';
 const AccountDeleted = ({ clientHistory }) => {
   return (
     <S.Wrapper>
-      <Title boldSection="delete" lightSection="Your account has been" />
+      <Title boldSection="deleted" lightSection="Your account has been" />
 
       <Row mb="6" mt="5">
         <Col w={[4, 8, 8]}>
-          <T.P color="gray8">Your therapist has been notified</T.P>
+          <T.P color="gray8">Your therapist has been notified.</T.P>
         </Col>
       </Row>
       <Row>
