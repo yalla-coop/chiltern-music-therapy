37d996bc827e73f8bd6f6e77b93663fc48623c02
Ramy Shurafa <ramyshurafa@hotmail.com>
2021-04-08 13:49:09 +0300

    create email templates

--- server/modules/user/use-cases/delete-user.js
+++ server/modules/user/use-cases/delete-user.js
@@ -4,15 +4,12 @@ import { userRoles as roles } from '../../../constants';
 import events from '../../../services/events';
 
 const deleteUser = async ({ id, role }) => {
+  let deletedUser;
+
   switch (role) {
     case roles.CLIENT:
-      await User.deleteUserDetails(id);
-      // email
-      // progress update
-      // media
-      // s3
-
-      events.emit(events.types.USER.CLIENT_DELETED, { userId: id });
+      deletedUser = await User.deleteUserDetails(id);
+      events.emit(events.types.USER.CLIENT_DELETED, deletedUser);
       break;
 
     default:
---
+++ server/services/emails/templates/client.account-deleted.js
@@ -0,0 +1,18 @@
+// Client deletes account
+// recipient => 'CLIENT'
+export default ({ firstInitial }) => {
+  const subject = 'You have successfully deleted your account!';
+
+  const html = `
+    <div>
+      <p>Dear ${firstInitial},</p>
+      <br>
+      <p>You have successfully deleted your account from Chiltern Music Therapy Digital Platform</p>
+      <br>
+      <p>Best wishes</p>
+      <p>Chiltern Music Therapy</p>
+    </div>
+  `;
+
+  return { html, subject };
+};
