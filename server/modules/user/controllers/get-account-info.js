d9856ab3907daf0784ac379110e3f9e0185e05bf
Joe Friel <joseph.s.friel@gmail.com>
2021-04-12 11:12:51 +0100

    Update server/modules/user/controllers/get-account-info.js
    Co-authored-by: RamyAlshurafa <34629478+RamyAlshurafa@users.noreply.github.com>

--- server/modules/user/controllers/get-account-info.js
+++ server/modules/user/controllers/get-account-info.js
@@ -2,7 +2,6 @@ import * as User from '../use-cases';
 
 const getAccountInfo = async (req, res, next) => {
   const { id, role } = req.user;
-  console.log('hey', id, role);
   try {
     const accountInfo = await User.getAccountInfo({ id, role });
     res.json(accountInfo);
