bbf1ba888a2d7ac5b376284325044f0aa5f46499
thejoefriel <joseph.s.friel@gmail.com>
2021-04-12 18:22:33 +0100

    Fix title weight > #119

--- client/src/components/Title/index.js
+++ client/src/components/Title/index.js
@@ -23,7 +23,7 @@ const Title = ({
       <Col w={[4, 8, 8]} style={{ display: 'flex ' }}>
         <T.H1>
           {lightSection}{' '}
-          <span style={{ fontWeight: 'bold' }}>{boldSection}</span>
+          <span style={{ fontWeight: '900' }}>{boldSection}</span>
           {lightSection2 && ` ${lightSection2}`}
         </T.H1>
       </Col>
