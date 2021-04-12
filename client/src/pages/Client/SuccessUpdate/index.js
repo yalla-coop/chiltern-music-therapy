dff0f9b32b3ad2cf1912742eb4394936f42589e6
fadi <fadeomar2015@gmail.com>
2021-04-12 15:47:10 +0300

    update success update page

--- client/src/pages/Client/SuccessUpdate/index.js
+++ client/src/pages/Client/SuccessUpdate/index.js
@@ -3,12 +3,22 @@ import Button from '../../../components/Button';
 import Title from '../../../components/Title';
 import * as T from '../../../components/Typography';
 import { CLIENT } from '../../../constants/nav-routes';
+import { useAuth } from '../../../context/auth';
 
 const SuccessUpdate = () => {
-  return (
-    <>
+  const {
+    user: { role },
+  } = useAuth();
+  return role === 'THERAPIST' ? (
+    <Title
+      boldFirst
+      boldSection="Success!"
+      lightSection="Your feedback has been sent"
+    />
+  ) : (
+    <Title>
       <Title boldSection="Thanks" lightSection="for your update!" boldFirst />
-      <Row>
+      <Row style={{ border: '1px solid red', flexGrow: 1 }}>
         <Col w={[4, 4, 4]}>
           <T.P color="gray8" mb="4">
             Your therapist will now receive an update.
@@ -16,7 +26,7 @@ const SuccessUpdate = () => {
           <Button variant="primary" text="Return home" to={CLIENT.DASHBOARD} />
         </Col>
       </Row>
-    </>
+    </Title>
   );
 };
 
