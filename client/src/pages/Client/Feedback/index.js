87f987ec02959e64ad1b843a3697beb0883209f2
fadi <fadeomar2015@gmail.com>
2021-04-14 16:32:04 +0300

    create feedback page

---
+++ client/src/pages/Client/Feedback/index.js
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import { CLIENT } from '../../../constants/nav-routes';
+import { sendFeedback } from '../../../api-calls/progress-updates';
+
+import Q1 from './Q1';
+import Q2 from './Q2';
+import Q3 from './Q3';
+import Q4 from './Q4';
+
+const CurrentQuestion = ({ currentQ, handleStep, handleFinalStep }) => {
+  if (currentQ === 1) {
+    return <Q1 handleStep={handleStep} />;
+  }
+  if (currentQ === 2) {
+    return <Q2 handleStep={handleStep} />;
+  }
+  if (currentQ === 3) {
+    return <Q3 handleStep={handleStep} />;
+  }
+  if (currentQ === 4) {
+    return <Q4 handleFinalStep={handleFinalStep} />;
+  }
+  return null;
+};
+
+const Feedback = () => {
+  const history = useHistory();
+  const { id } = useParams();
+  const [currentQ, setCurrentQ] = useState(1);
+  const [state, setState] = useState({});
+
+  const handleStep = (stepData) => {
+    setState((oldState) => ({ ...oldState, ...stepData }));
+    setCurrentQ((prevQ) => prevQ + 1);
+  };
+
+  const handleFinalStep = async (stepData) => {
+    await sendFeedback({
+      ...state,
+      ...stepData,
+      programmeId: id,
+    });
+    history.push(CLIENT.SUCCESS_FEEDBACK);
+  };
+
+  return (
+    <>
+      <CurrentQuestion
+        currentQ={currentQ}
+        handleStep={handleStep}
+        handleFinalStep={handleFinalStep}
+      />
+    </>
+  );
+};
+
+export default Feedback;
