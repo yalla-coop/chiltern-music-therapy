0f28a43028fd63523f09b6885cc6df599a471dc3
Ramy Shurafa <ramyshurafa@hotmail.com>
2021-04-14 15:22:32 +0300

    authorise signed url endpoint

--- server/modules/media/use-cases/get-media-url.js
+++ server/modules/media/use-cases/get-media-url.js
@@ -1,6 +1,14 @@
+import Boom from '@hapi/boom';
 import { getFilePreSignedUrl as getMediaUrlService } from '../../../services/files-storage';
+import { errorMsgs } from '../../../services/error-handler';
 
-const getMediaUrl = async ({ key, bucket }) => {
+const getMediaUrl = async ({ userId, key, bucket }) => {
+  const keyUserId = key.split('/')[0];
+
+  if (Number(keyUserId) !== Number(userId)) {
+    throw Boom.unauthorized(errorMsgs.UNAUTHORISED);
+  }
+
   const mediaUrl = await getMediaUrlService({
     key,
     bucket,
