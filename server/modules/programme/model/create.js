65228d3d8ae797888377d13b6c4a9701a13fb6a2
RamyAlshurafa <34629478+RamyAlshurafa@users.noreply.github.com>
2021-04-15 01:08:39 +0300

    send feedback backend (#176)
    * send feedback backend
    
    * fix redirection after add client

--- server/modules/programme/model/create.js
+++ server/modules/programme/model/create.js
@@ -33,4 +33,52 @@ const createProgrammesContent = async ({ programmeId, contentId }, client) => {
   return res.rows[0];
 };
 
-export { createProgramme, createProgrammesContent };
+const createProgrammeFeedback = async (
+  {
+    programmeId,
+    clearInstructions,
+    problems,
+    clearDemos,
+    noDemos,
+    enjoyableResources,
+    likeMostAndLeast,
+  },
+  client,
+) => {
+  const sql = `
+    INSERT INTO programmes_feedbacks(
+      programme_id,
+      clear_instructions,
+      problems,
+      clear_demos,
+      no_demos,
+      enjoyable_resources,
+      like_most_and_least
+    )
+      VALUES (
+        $1,
+        $2,
+        $3,
+        $4,
+        $5,
+        $6,
+        $7
+      ) RETURNING id
+  `;
+  const res = await query(
+    sql,
+    [
+      programmeId,
+      clearInstructions,
+      problems,
+      clearDemos,
+      noDemos,
+      enjoyableResources,
+      likeMostAndLeast,
+    ],
+    client,
+  );
+  return res.rows[0];
+};
+
+export { createProgramme, createProgrammesContent, createProgrammeFeedback };
