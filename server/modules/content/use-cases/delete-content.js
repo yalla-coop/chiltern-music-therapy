9c7177f3eb809daf57861c4b8c00ff0bdfdbb6fd
Simon Dupree <dupreenator@gmail.com>
2021-04-15 11:09:15 +0200

    updated delete media logic

--- server/modules/content/use-cases/delete-content.js
+++ server/modules/content/use-cases/delete-content.js
@@ -17,29 +17,34 @@ const deleteContent = async ({ id, role, userId, mode }, client) => {
     throw Boom.unauthorized(errorMsgs.UNAUTHORISED_EDIT);
   }
 
-  const delCats = await Content.deleteContentCategories(id, client);
-  const delContProgramme = await Content.deleteContentFromProgramme(id, client);
-  const delContent = await Content.deleteContentById(id, client);
-
-  if (delCats && delContProgramme && delContent) {
+  const delContent = async () => {
+    const fns = {};
+    fns.contentCats = await Content.deleteContentCategories(id, client);
+    fns.contentProg = await Content.deleteContentFromProgramme(id, client);
+    fns.content = await Content.deleteContentById(id, client);
     // check if media is used anywhere else. if not then delete
-    events.emit(events.types.MEDIA.CONTENT_DELETED, {
+    fns.media = await events.emit(events.types.MEDIA.CONTENT_DELETED, {
       mediaId: contentToDelete.mediaId,
       contentId: id,
     });
-  }
+    return fns;
+  };
 
-  // for library deletions page depends on updated content object
-  // for remove from programme not
-  if (mode === 'library') {
-    const updatedContent = await getLibraryContent(
-      { id: userId, role },
-      client,
-    );
-    return updatedContent;
-  }
+  const deletedData = await delContent();
 
-  return 'success';
+  if (deletedData && deletedData.media) {
+    // for library deletions page depends on updated content object
+    // for remove from programme not
+    if (mode === 'library') {
+      const updatedContent = await getLibraryContent(
+        { id: userId, role },
+        client,
+      );
+      return updatedContent;
+    }
+
+    return 'success';
+  }
 };
 
 export default deleteContent;
