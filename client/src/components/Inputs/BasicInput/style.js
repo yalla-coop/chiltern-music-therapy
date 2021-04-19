6d0a3a37881f0fd418662117e2d492a89822ba13
fadi <fadeomar2015@gmail.com>
2021-04-19 14:39:15 +0300

    show the eye on password inputs

--- client/src/components/Inputs/BasicInput/style.js
+++ client/src/components/Inputs/BasicInput/style.js
@@ -1,28 +1,41 @@
 import styled from '@emotion/styled';
 import setMargin from '../../../helpers/set-margin';
 import * as CS from '../style';
+import { Input as AntdInput } from 'antd';
+
+const InputCommonStyle = ({ theme, error, disabled }) => `
+border: ${theme.borders.inputs};
+border-color: ${error ? theme.colors.error : theme.colors.gray5};
+border-radius: ${theme.borders.radius};
+
+width: 100%;
+padding: ${theme.spacings[3]};
+line-height: 24px;
+
+color: ${theme.colors.gray9};
+font-size: 16px;
+
+cursor: ${disabled && 'not-allowed'};
+background: ${theme.colors.gray2};
+
+:focus {
+  outline: none;
+  border-color: ${theme.colors.blue};
+}
+`;
 
 export const Input = styled.input`
   ${setMargin};
   ${CS.placeholderStyle};
-  border: ${({ theme }) => theme.borders.inputs};
-  border-color: ${({ theme, error }) =>
-    error ? theme.colors.error : theme.colors.gray5};
-  border-radius: ${({ theme }) => theme.borders.radius};
+  ${InputCommonStyle};
+`;
 
-  width: 100%;
-  padding: ${({ theme }) => theme.spacings[3]};
-  line-height: 24px;
-
-  color: ${({ theme }) => theme.colors.gray9};
-  font-size: 16px;
-
-  cursor: ${({ disabled }) => disabled && 'not-allowed'};
-  background: ${({ theme }) => theme.colors.gray2};
-
-  :focus {
-    outline: none;
-    border-color: ${({ theme }) => theme.colors.blue};
+export const PasswordInput = styled(AntdInput.Password)`
+  ${setMargin};
+  ${CS.placeholderStyle};
+  ${InputCommonStyle};
+  input {
+    background-color: inherit;
   }
 `;
 
