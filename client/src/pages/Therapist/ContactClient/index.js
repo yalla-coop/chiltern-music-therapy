5b5c97b199cc8ee81f40cebdc35d5f0db9a536b4
fadi <fadeomar2015@gmail.com>
2021-04-20 00:12:25 +0300

    add color for page subtitles

--- client/src/pages/Therapist/ContactClient/index.js
+++ client/src/pages/Therapist/ContactClient/index.js
@@ -39,25 +39,25 @@ const ContactClient = () => {
       <Title boldSection="Contact" lightSection="My Client" boldFirst />
       <Row>
         <Col w={[4, 4, 4]} display="block">
-          <T.H5>CLIENT NAME</T.H5>
+          <T.H5 color="gray7">CLIENT NAME</T.H5>
           <T.P mt="2">
             {state.firstInitial
               ? `${state.firstInitial} ${state.lastInitial}`
               : 'N/A'}{' '}
           </T.P>
         </Col>
-        <Col w={[4, 4, 4]} display="block">
-          <T.H5>CONTACT EMAIL</T.H5>
+        <Col w={[4, 4, 4]} mtT={5} display="block">
+          <T.H5 color="gray7">EMAIL</T.H5>
           <T.P mt="2">{state.contactEmail}</T.P>
         </Col>
-        <Col w={[4, 4, 4]} display="block">
-          <T.H5>CONTACT NUMBER</T.H5>
+        <Col w={[4, 4, 4]} mtT={5} display="block">
+          <T.H5 color="gray7">MOBILE NUMBER</T.H5>
           <T.P mt="2">{state.contactNumber} </T.P>
         </Col>
       </Row>
       {errors && (
         <Row>
-          <Col w={[4, 12, 12]}>
+          <Col w={[4, 12, 12]} mt={5}>
             <T.P color="pink">{errors}</T.P>
           </Col>
         </Row>
