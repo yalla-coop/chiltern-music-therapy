5448144d88f22d273bcff182bd3c765596d7c11b
fadi <fadeomar2015@gmail.com>
2021-04-21 13:31:22 +0300

    update the contet style

--- client/src/components/Cards/Expandable/style.js
+++ client/src/components/Cards/Expandable/style.js
@@ -41,6 +41,11 @@ export const Title = styled.div`
   transition: all 500ms ease-in;
   display: flex;
   align-items: center;
+  position: relative;
+  z-index: ${({ open }) => (open ? -1 : 1)};
+  div {
+    height: ${({ open }) => open && 0};
+  }
 `;
 
 export const Content = styled.div`
@@ -85,6 +90,7 @@ export const BoldSpan = styled.span`
 `;
 
 export const Anchor = styled.a`
+  position: relative;
   z-index: 2;
   -webkit-transform: translate3d(0, 0, 0);
   transform: translate3d(0, 0, 0);
