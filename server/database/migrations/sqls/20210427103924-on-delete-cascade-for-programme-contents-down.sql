0623dadb3889e3c5396899902c1cc5c37f11c0fc
Ramy Shurafa <ramyshurafa@hotmail.com>
2021-04-28 15:22:03 +0300

    add on delete cascade hook for prog_contents

---
+++ server/database/migrations/sqls/20210427103924-on-delete-cascade-for-programme-contents-down.sql
@@ -0,0 +1,22 @@
+BEGIN;
+
+ALTER TABLE programmes_contents
+DROP CONSTRAINT programmes_contents_programme_id_fkey,
+ADD CONSTRAINT programmes_contents_programme_id_fkey
+  FOREIGN KEY (programme_id)
+  REFERENCES programmes(id);
+
+ALTER TABLE programmes_contents ALTER programme_id SET NOT NULL;
+
+ALTER TABLE programmes_contents
+DROP CONSTRAINT programmes_contents_content_id_fkey,
+ADD CONSTRAINT programmes_contents_content_id_fkey
+  FOREIGN KEY (content_id)
+  REFERENCES contents(id);
+
+
+ALTER TABLE programmes_contents ALTER content_id SET NOT NULL;
+
+
+
+COMMIT;
--- server/modules/content/use-cases/delete-content.js
+++ server/modules/content/use-cases/delete-content.js
@@ -17,18 +17,13 @@ const deleteContent = async ({ id, role, userId, mode }, client) => {
     throw Boom.unauthorized(errorMsgs.UNAUTHORISED_EDIT);
   }
 
-  const deleted = await Promise.all([
-    Content.deleteContentCategories(id, client),
-    Content.deleteContentFromProgramme(id, client),
-    Content.deleteContentById(id, client),
-  ]);
-  if (deleted) {
-    // check if media is used anywhere else. if not then delete
-    events.emit(events.types.MEDIA.CONTENT_DELETED, {
-      mediaId: contentToDelete.mediaId,
-      contentId: id,
-    });
-  }
+  await Content.deleteContentById(id, client);
+
+  // check if media is used anywhere else. if not then delete
+  events.emit(events.types.MEDIA.CONTENT_DELETED, {
+    mediaId: contentToDelete.mediaId,
+    contentId: id,
+  });
 
   // for library deletions page depends on updated content object
   // for remove from programme not
