088c3e66533da3bd09cc2842b6eb8346e85693e7
RamyAlshurafa <34629478+RamyAlshurafa@users.noreply.github.com>
2021-04-28 19:59:19 +0300

    add on delete cascade hook for prog_contents (#240)

--- server/database/models/programmes-contents/schema.sql
+++ server/database/models/programmes-contents/schema.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS "programmes_contents" CASCADE;
 
 CREATE TABLE "programmes_contents" (
   "id" SERIAL PRIMARY KEY,
-  "programme_id" INTEGER REFERENCES programmes(id) NOT NULL,
-  "content_id" INTEGER REFERENCES contents(id) NOT NULL,
+  "programme_id" INTEGER REFERENCES programmes(id) ON DELETE CASCADE NOT NULL,
+  "content_id" INTEGER REFERENCES contents(id) ON DELETE CASCADE NOT NULL,
   "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
   "updated_at" TIMESTAMP NOT NULL DEFAULT NOW()
 );
