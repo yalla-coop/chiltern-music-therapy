3ff762d5ac9c939c4377fd6d5f9160da97f6adcd
Ramy Shurafa <ramyshurafa@hotmail.com>
2021-05-06 01:35:31 +0300

    show recaptcha policy on sign up pages

--- client/src/pages/Client/Signup/index.js
+++ client/src/pages/Client/Signup/index.js
@@ -11,6 +11,7 @@ import { navRoutes } from '../../../constants';
 import { useAuth } from '../../../context/auth';
 import validate from '../../../validation/schemas/signup';
 import { roles } from './../../../constants';
+import ReCaptcha from '../../../components/ReCaptcha';
 
 import { Users, TherapistClients } from '../../../api-calls';
 
@@ -259,6 +260,11 @@ const ClientSignup = ({ status, title, msg }) => {
             type="submit"
             loading={loading}
           />
+        </Col>
+      </Row>
+      <Row>
+        <Col w={[4, 12, 4]}>
+          <ReCaptcha />
         </Col>
       </Row>
     </S.Wrapper>
