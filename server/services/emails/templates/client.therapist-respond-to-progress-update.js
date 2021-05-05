e696139ef0d59f490c60f3b334c93fd25ce73c5c
Ramy Shurafa <ramyshurafa@hotmail.com>
2021-05-05 16:45:15 +0300

    fix #249

--- server/services/emails/templates/client.therapist-respond-to-progress-update.js
+++ server/services/emails/templates/client.therapist-respond-to-progress-update.js
@@ -11,7 +11,7 @@ export default ({ firstInitial, therapistName }) => {
     <div>
       <p>Dear ${firstInitial},</p>
       <br>
-      <p>Your Music Therapist ${therapistName}, has sent you a response to your feedback.</p>
+      <p>Your Music Therapist ${therapistName}, has sent you a response to your update.</p>
       <p>Follow this ${loginLink} to log in and view his/her comments!.</p>
       <p>${appLinks.LOGIN}</p>
       <br>
