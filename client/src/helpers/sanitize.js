abc58ffd6713594222c4e831350b1ed0aecff8e9
fadi <fadeomar2015@gmail.com>
2021-06-11 20:15:42 +0300

    rename the parameters

--- client/src/helpers/sanitize.js
+++ client/src/helpers/sanitize.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-useless-escape */
-const sanitize = (string, test, password) => {
+const sanitize = (string, isTest, isPassword) => {
   const rgx = /([-!#$%^&*=+/(){}\][:\'\"\`<>?\,])/g;
   const rgx2 = /([/(){}\][:\'\"\`<>?\,])/g;
   if (typeof string !== 'string') return;
-  if (test) {
+  if (isTest) {
     const result = string.match(rgx);
     return result && result.length > 0 ? true : null;
   }
 
-  if (password) {
+  if (isPassword) {
     return string.replace(rgx2, '');
   }
   return string.replace(rgx, '');
