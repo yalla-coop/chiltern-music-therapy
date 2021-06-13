2a5934ec84a50b1510f8cdaa2198d6b967b74c11
RamyAlshurafa <34629478+RamyAlshurafa@users.noreply.github.com>
2021-06-13 13:23:38 +0300

    config csp in helmet middleware (#266)

--- server/api/middlewares/index.js
+++ server/api/middlewares/index.js
@@ -1,5 +1,6 @@
 import authenticate from './authenticate';
 import authorize from './authorize';
 import requireHTTPS from './require-https';
+import helmet from './helmet';
 
-export { authenticate, authorize, requireHTTPS };
+export { authenticate, authorize, requireHTTPS, helmet };
